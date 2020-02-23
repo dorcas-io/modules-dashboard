c68dda2a940586c097d92f1c77e8cc327140a5f6
Ifeoluwa <bolaji.olawoye@hostvillenigeria.org>
2020-02-23 11:58:55 +0100

    Resend Event

--- src/Http/Controllers/ModulesDashboardController.php
+++ src/Http/Controllers/ModulesDashboardController.php
@@ -540,4 +540,24 @@ class ModulesDashboardController extends Controller {
     }
 
 
+    /**
+     * @param Request $request
+     * @param Sdk     $sdk
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function resendVerification(Request $request, Sdk $sdk)
+    {
+        $user = $request->user();
+        # get the authenticated user
+        $query = $sdk->createUserResource($user->id)->send('POST', ['resend-verification']);
+        # send the request
+        if (!$query->isSuccessful()) {
+            throw new \RuntimeException($query->errors[0]['title'] ?? 'Could not resend the verification email.');
+        }
+        return response()->json($query->getData());
+    }
+
+
 }
