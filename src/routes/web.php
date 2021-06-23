9d188794b4abe4c8c51c88539f8ef1832264b54b
Ifeoluwa Olawoye <bolaji.olawoye@hostvillenigeria.org>
2021-06-23 23:52:14 +0100

    features update route fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,7 +5,7 @@ Route::group(['namespace' => 'Dorcas\ModulesDashboard\Http\Controllers', 'prefix
 	Route::get('/setup', 'ModulesDashboardController@welcome_setup')->name('welcome-setup');
 	Route::post('/setup', 'ModulesDashboardController@welcome_post');
 	Route::post('/features', 'ModulesDashboardController@welcome_features')->name('save-dashboard-features');
-	Route::get('/features', 'ModulesDashboardController@welcome_features')->name('save-dashboard-features2');
+	Route::get('/features2', 'ModulesDashboardController@welcome_features')->name('save-dashboard-features2');
 	Route::get('/overview', 'ModulesDashboardController@welcome_overview')->name('welcome-overview');
 	Route::post('/resend-verification', 'ModulesDashboardController@resendVerification');
 });
