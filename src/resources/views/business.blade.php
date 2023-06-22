834aa46ea418af166f7b65d1043e5594e74cb9ac
Ifeoluwa Olawoye <bolaji.olawoye@hostvillenigeria.org>
2023-06-22 19:06:04 +0100

    checklists update

--- src/resources/views/business.blade.php
+++ src/resources/views/business.blade.php
@@ -180,8 +180,8 @@
                                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="1" /><circle cx="12" cy="19" r="1" /><circle cx="12" cy="5" r="1" /></svg>
                                         </a>
                                         <div class="dropdown-menu dropdown-menu-end">
-                                            <!-- <a href="#" class="dropdown-item">Import</a> -->
-                                            <a href="#" class="dropdown-item text-danger" v-on:click="dashboardPanelRemove">Remove</a>
+                                            <a href="#" class="dropdown-item">Status</a>
+                                            <a href="#" class="dropdown-item text-danger" v-on:click.prevent="dashboardPanelRemove">Remove</a>
                                         </div>
                                     </div>
                                 </div>
